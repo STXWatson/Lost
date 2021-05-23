504bdac1f6b8f47174c195343aa66f6326ffcab5
STXWatson <73170773+STXWatson@users.noreply.github.com>
2021-05-23 11:18:34 +0100

    Changes to functionality and structure
    * Function structure simplified to assess non-special input in check_txt
    * Response to say when there's nothing to see
    * n, s, e, w changed to north, south, east west
    * "help" repeats initial help
    * "where" repeats location txt
    * "doors" repeats list of doors
    * Pocket now a dictionary with Boolean values, so they can be "used". Only visible in pocket when True.
    * "You can't go in that direction" response if direction entered that is not available in that room

--- aventure_game.py
+++ aventure_game.py
@@ -8,15 +8,28 @@ def intro():
     print("This is where you introduce your game and say hello.")
     print("Perhaps add some pauses for effect...")
     sleep(2)
-    print("""You can type "n", "s", "e", "w" to move north, south, east and west.""")
+    help_txt()
+    print("\n")
+
+    # The pocket is a dictionary of items you carry with you
+    # If an item is set to "True" they are displayed when you type "pocket"
+    # If false they don't appear anymore e.g. you can use or eat them.
+    pocket = {"Just some pocket fluff": True}
+
+    # flags is a dictionary that can store when something special happens
+    # Allow something you do in one room to affect other rooms
+    flags = {"flag1": False}
+
+    room_1(pocket, flags)   # this is your starting room
+
+def help_txt():
+    print("""You can type "north", "south", "east", "west" to move in that direction.""")
     print("""You can type "pocket" to see what rubbish is in there.""")
     print("""Type "look" to see what objects are available and the name of the object to take it.""")
     print("""Some items can be used once you pick them up by typing "use" and then the item name.""")
+    print("""You can type "where" or "doors" to repeat the location information for that room.""")
+    print("""If you forget any of this type "help" to hear it all again.""")
     print("""Finally, you can type "exit" when you get bored!""")
-
-    pocket = ["Just some pocket fluff"]   # This is a list of what your starting pocket contains
-    flags = {"flag1": False}   # This is a dictionary that stores when special stuff happens
-    room_1(pocket, flags)   # this is your starting room
 
 #######################################################
 ### these are the general functions used everywhere ###
@@ -24,17 +37,18 @@ def intro():
 
 def open_pocket(pocket):
 # displays what's in yer pocket #
-    for item in pocket:
-        print(item)
+    for item in pocket.keys():
+        if pocket[item]:  # only prints item if it is tagged as True
+            print(item)
     return
 
 def items_available(initial_items, pocket):
 # looks at the initial items and ignores anything in your pocket
-    available_items = [item for item in initial_items if item not in pocket]
+    available_items = [item for item in initial_items if item not in pocket.keys()]
     return available_items
 
-def check_txt(txt, initial_items, pocket):
-# general function to respond to words pocket, exit, look, exit and the name of items
+def check_txt(txt, initial_items, pocket, doors, flags, location_text, door_text):
+# general function to respond to words pocket, exit, look, exit, etc
 
     if txt == "pocket":
         open_pocket(pocket)
@@ -42,24 +56,28 @@ def check_txt(txt, initial_items, pocket):
         print("Back to normality!")
         sys.exit()
     elif txt == "look":
-        print("There is:")
-        for item in items_available(initial_items, pocket):
-            print(item)
+        print("You can see:")
+        if len(items_available(initial_items, pocket)) != 0:  #print any available items
+            for item in items_available(initial_items, pocket):
+                print(item)
+        else:
+            print("Absolutely nothing.")  # if no items available, print this!
     elif txt in items_available(initial_items, pocket):
         print("You take the", txt)
-        pocket.append(txt)
+        pocket[txt] = True
+    elif txt == "where":
+        print(location_text)
+    elif txt == "doors":
+        print(door_text)
+    elif txt in doors.keys():
+        eval(doors[txt] + "(pocket, flags)")
+    elif txt in ["north", "south", "east", "west"] and txt not in doors.keys():
+        print("You can't go in that direction.")
+    elif txt == "help":
+        help_txt()
     else:
         print("?")
     return pocket
-
-def directions(doors, txt, pocket, flags):
-# if the user types n, s, e, w this pulls the name of the next room from the room dictionary
-# and then jumps to that room
-    if txt in doors.keys():
-        eval(doors[txt] + "(pocket, flags)")
-    else:
-        return
-
 
 ###########################################################
 ### These are the functions for the different locations ###
@@ -67,54 +85,61 @@ def directions(doors, txt, pocket, flags):
 
 def room_1(pocket, flags):
 
+    location_text = "Welcome to room 1."
+    door_txt = "There is a door to the south."
     initial_items = ["item 1"]
-    doors = {"s": "room_2"}
+    doors = {"south": "room_2"}
 
-    print("Welcome to room 1.")
+    print(location_text)
     sleep(1)
-    print("There is a door to the south.")
+    print(door_txt)
 
     while True:
         txt = input(">")
-        directions(doors, txt, pocket, flags)
-        pocket = check_txt(txt, initial_items, pocket)
+        pocket = check_txt(txt, initial_items, pocket, doors, flags, location_text, door_txt)
 
         # section for special item use
-        if txt == "use item 1" and "item 1" in pocket:
-            print("you have now used item 1 in the right place!")
+        if txt == "use item 1" and "item 1" in pocket.keys() and pocket.get("item 1"):
+            print("You have now used item 1 in the right place!")
+            print("It now vanishes mysteriously")
             flags["flag1"] = True
+            pocket["item 1"] = False
 
 def room_2(pocket, flags):
 
+    location_text = "Welcome to room 2."
+    door_txt = "There are doors to the north and west."
     initial_items = ["item 2"]
-    doors = {"n": "room_1", "w": "room_3"}
+    doors = {"north": "room_1", "west": "room_3"}
 
-    print("Welcome to room 2.")
+    print(location_text)
     sleep(1)
-    print("There are doors to the north and west.")
+    print(door_txt)
 
     while True:
         txt = input(">")
-        directions(doors, txt, pocket, flags)
-        pocket = check_txt(txt, initial_items, pocket)
+        pocket = check_txt(txt, initial_items, pocket, doors, flags, location_text, door_txt)
 
         # section for special item use
         if txt == "use item 2" and flags["flag1"]:
             print("I see you have used item 1 in room 1, you can now use item 2!")
-        elif txt == "use item 2" and not flags["flag1"]:
+        elif txt == "use item 2" and "item 2" in pocket.values() and not flags["flag1"]:
             print("I see you haven't used item 1 in room 1 yet, so you can't use item 2.")
 
 def room_3(pocket, flags):
 
+    location_text = "Welcome to room 3."
+    door_txt = "There is a door to the east."
     initial_items = ["item 3"]
-    doors = {"e": "room_2"}
+    doors = {"east": "room_2"}
+
+    print(location_text)
     sleep(1)
-    print("There is a door to the east.")
+    print(door_txt)
 
     while True:
         txt = input(">")
-        directions(doors, txt, pocket, flags)
-        pocket = check_txt(txt, initial_items, pocket)
+        pocket = check_txt(txt, initial_items, pocket, doors, flags, location_text, door_txt)
 
     # section for special item use
 
